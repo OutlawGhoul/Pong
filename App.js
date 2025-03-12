615537b29f01aa6adc19fa881de3b5d805b8de1a
Steven Schwarz <steven.schwarz@twofold.swiss>
2025-03-12 15:58:36 +0000

    Replace App.js

--- App.js
+++ App.js
@@ -3,93 +3,96 @@ import './App.css';
 
 const App = () => {
 	const ballRadius = 10;
-	const initialBallState = { x: 300, y: 200, speedX: 5, speedY: 5 };
+	const initialBallState = { x: 294, y: 195, speedX: 5, speedY: 5 };
 	const initialPaddleState = { left: 150, right: 150 };
+	const [ball, setBall] = useState(initialBallState);
+	const [paddles, setPaddles] = useState(initialPaddleState);
 	const [gameOver, setGameOver] = useState(false);
 	const [gameRunning, setGameRunning] = useState(false);
+
 	const containerHeight = 390;
 	const containerWidth = 588;
 	const paddleHeight = 90;
 	const paddleWidth = 15;
 
-	const ballRef = useRef(initialBallState);
-	const paddlesRef = useRef(initialPaddleState);
 	const intervalRef = useRef(null);
+  const paddlesRef = useRef(initialPaddleState);
 
 	const updateBallPosition = () => {
-		const ball = ballRef.current;
-		const paddles = paddlesRef.current;
-		ball.x += ball.speedX;
-		ball.y += ball.speedY;
+		setBall((prevBall) => {
+			let updateBall = { ...prevBall };
+			updateBall.x += updateBall.speedX;
+			updateBall.y += updateBall.speedY;
 
-		const ballLeft = ball.x - ballRadius;
-		const ballRight = ball.x + ballRadius;
-		const ballTop = ball.y - ballRadius;
-		const ballBottom = ball.y + ballRadius;
+			const ballLeft = updateBall.x - ballRadius;
+			const ballRight = updateBall.x + ballRadius;
+			const ballTop = updateBall.y - ballRadius;
+			const ballBottom = updateBall.y + ballRadius;
 
-		const paddleLeftTop = paddles.left;
-		const paddleLeftBottom = paddles.left + paddleHeight;
-		const paddleRightTop = paddles.right;
-		const paddleRightBottom = paddles.right + paddleHeight;
+			const paddleLeftTop = paddlesRef.current.left;
+			const paddleLeftBottom = paddlesRef.current.left + paddleHeight;
+			const paddleRightTop = paddlesRef.current.right;
+			const paddleRightBottom = paddlesRef.current.right + paddleHeight;
 
-		if (
-			ballLeft <= paddleWidth &&
-			ballRight >= 0 &&
-			ballTop < paddleLeftBottom &&
-			ballBottom > paddleLeftTop
-		) {
-			ball.speedX = -ball.speedX;
-			ball.speedY = ball.speedY + (Math.random() * 2 - 1);
-		}
+			if (ballLeft <= paddleWidth &&
+			  	ballTop < paddleLeftBottom &&
+				  ballBottom > paddleLeftTop) {
+			  updateBall.speedX = -updateBall.speedX;
+			  updateBall.speedY = updateBall.speedY + (Math.random() * 2 - 1);
+			}
 
-		if (
-			ballRight >= containerWidth - paddleWidth &&
-			ballLeft <= containerWidth &&
-			ballTop < paddleRightBottom &&
-			ballBottom > paddleRightTop
-		) {
-			ball.speedX = -ball.speedX;
-			ball.speedY = ball.speedY + (Math.random() * 2 - 1);
-		}
+			if (ballRight >= containerWidth - paddleWidth &&
+				ballTop < paddleRightBottom &&
+				ballBottom > paddleRightTop) {
+			  updateBall.speedX = -updateBall.speedX;
+			  updateBall.speedY = updateBall.speedY + (Math.random() * 2 - 1);
+			}
 
-		if (ballTop <= 0) {
-			ball.speedY = -ball.speedY;
-			ball.y = ballRadius;
-		}
+			if (ballTop <= 0) {
+				updateBall.speedY = -updateBall.speedY;
+				updateBall.y = ballRadius;
+			}
 
-		if (ballBottom >= containerHeight) {
-			ball.speedY = -ball.speedY;
-			ball.y = containerHeight - ballRadius;
-		}
+			if (ballBottom >= containerHeight) {
+				updateBall.speedY = -updateBall.speedY;
+				updateBall.y = containerHeight - ballRadius;
+			}
 
-		if (ballLeft < 0 || ballRight > containerWidth) {
-			setGameOver(true);
-			pauseGame();
-			ballRef.current = initialBallState;
-		}
+			if (ballLeft < 0 || ballRight > containerWidth) {
+				setGameOver(true);
+				pauseGame();
+				setBall(initialBallState);
+			}
 
-		forceUpdate();
+			return updateBall;
+		});
 	};
 
 	const handleKeyPress = (e) => {
-		const paddles = paddlesRef.current;
-		switch (e.key) {
-			case 'ArrowUp' :
-				paddles.right = Math.max(paddles.right - 10, 0);
-				break;
-			case 'ArrowDown':
-				paddles.right = Math.min(paddles.right + 10, containerHeight - paddleHeight);
-				break;
-			case 'w':
-				paddles.left = Math.max(paddles.left - 10, 0);
-				break;
-			case 'd':
-				paddles.left = Math.min(paddles.left + 10, containerHeight - paddleHeight);
-				break;
-			default:
-				break;
-		}
-		forceUpdate();
+		setPaddles((prevPaddles) => {
+			let updatePaddles = { ...prevPaddles };
+			switch (e.key) {
+				case 'ArrowUp' :
+					updatePaddles.right = Math.max(updatePaddles.right - 10, 0);
+          paddlesRef.current.right = updatePaddles.right;
+					break;
+				case 'ArrowDown':
+					updatePaddles.right = Math.min(updatePaddles.right + 10, containerHeight - paddleHeight);
+          paddlesRef.current.right = updatePaddles.right;
+					break;
+				case 'w':
+					updatePaddles.left = Math.max(updatePaddles.left - 10, 0);
+          paddlesRef.current.left = updatePaddles.left;
+					break;
+				case 'd':
+					updatePaddles.left = Math.min(updatePaddles.left + 10, containerHeight - paddleHeight);
+          paddlesRef.current.left = updatePaddles.left;
+					break;
+				default:
+					break;
+			}
+			return updatePaddles;
+		});
 	};
 
 	const startGame = () => {
@@ -104,15 +107,12 @@ const App = () => {
 	};
 
 	const restartGame = () => {
-		ballRef.current = initialBallState;
-		paddlesRef.current = initialPaddleState;
+		setBall(initialBallState);
+		setPaddles(initialPaddleState);
 		setGameOver(false);
 		setGameRunning(true);
 		intervalRef.current = setInterval(updateBallPosition, 50);
 	};
-
-	const [, forceUpdate] = useState(0);
-	const triggerUpdate = () => forceUpdate((x) => x + 1);
 
 	useEffect(() => {
 		if (gameRunning) {
@@ -129,16 +129,16 @@ const App = () => {
 			<div
 				className={`paddle paddle-left ${gameRunning ? '' : 'paused'}`}
 				id='paddle-left'
-				style={{ top: `${paddlesRef.current.left}px` }}
+				style={{ top: `${paddles.left}px` }}
 			/>
 			<div
 				className={`paddle paddle-right ${gameRunning ? '' : 'paused'}`}
 				id='paddle-right'
-				style={{ top: `${paddlesRef.current.right}px`, left: '580px' }}
+				style={{ top: `${paddles.right}px`, left: '580px' }}
 			/>
 			<div
 				className={`ball ${gameRunning ? '' : 'paused'}`}
-				style={{ top: `${ballRef.current.y}px`, left: `${ballRef.current.x}px` }}
+				style={{ top: `${ball.y}px`, left: `${ball.x}px` }}
 			/>
 			<div className='controls'>
 				<button onClick={startGame}>Start</button>
